f8a508ac804c1eb14b1a5b7bd535e22e868773a9
Emir Baran Kadirhan <ebk1013@gmail.com>
2026-03-03 22:59:53 +0300

    feat(frontend): add React UI with SearchBar and ResultCard components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import Searchbar from './components/Searchbar'
+import axios from 'axios';
+import ResultCard from './components/ResultCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, serError] = useState(null);
+
+  const handleSubmit = async (videoUrl) => {
+
+    setLoading(true)
+    try {
+      // axios ile backend'e istek at
+      const response = await axios.post("http://localhost:5000/api/transcript/get-transcript", {
+        videoUrl: videoUrl        // body'e videoUrl seklinde gonderiyoruz, controller tarafinda gonderileni aliyoruz !
+      })
+
+      setResult(response.data)
+      console.log(response.data)
+
+      // gelen veriyi setResult'a ver
+
+      setLoading(false)
+    } catch (error) {
+
+      console.log(error)
+    }
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+      <Searchbar onSubmit={handleSubmit} />
+      {result && <ResultCard transcriptData={result} />}
+
+    </div>
   )
 }
 
